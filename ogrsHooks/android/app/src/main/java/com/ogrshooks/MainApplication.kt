e87a2cf125eb6abec4975349a04ceca3da42f912
Vitor <vitorguilherme286@gmail.com>
2024-03-23 11:16:40 -0300

    first commit

--- ogrsHooks/android/app/src/main/java/com/ogrshooks/MainApplication.kt
+++ ogrsHooks/android/app/src/main/java/com/ogrshooks/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.orgshooks
+package com.ogrshooks
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ogrsHooks/ios/ogrsHooksTests/ogrsHooksTests.m
+++ ogrsHooks/ios/ogrsHooksTests/ogrsHooksTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface orgsHooksTests : XCTestCase
+@interface ogrsHooksTests : XCTestCase
 
 @end
 
-@implementation orgsHooksTests
+@implementation ogrsHooksTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
