643aaff9b204c44cbb572015f926dbe3027ee96f
Vitor <vitorguilherme286@gmail.com>
2024-04-01 17:02:01 -0300

    first commit

---
+++ Adopet/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+
+import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
+import Navigation from './src/rotas/navigation';
+
+export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    "PoppinsRegular": Poppins_400Regular,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <Navigation />
+  );
+}
---
+++ Adopet/src/rotas/navigation.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native/lib/commonjs";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "../paginas/Home";
+import Login from "../paginas/Login"
+
+const Stack = createStackNavigator();
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Login" component={Login} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
